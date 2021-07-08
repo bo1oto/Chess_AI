6f2a0a8fa519405502f725503b1a67d7f01ebe08
Vladislav <64910163+bo1oto@users.noreply.github.com>
2021-07-08 13:12:39 +0300

    Update DataTypes.fs

--- Code/DataTypes.fs
+++ Code/DataTypes.fs
@@ -36,11 +36,10 @@ type MoveFeature =
 | ForwardMove = 1uy//Pawn
 | Castling = 2uy//King, Rook
 | Transform = 3uy//Pawn
-| Sleep = 4uy//Фигура ещё не ходила
+| Sleep = 4uy//The figure has not walked yet
 | Check = 5uy
 | Mate = 6uy
 
-//Данные
 type FigureData = 
     {
         position: (int * int)
@@ -58,9 +57,9 @@ type FigureData =
 type PositionInfo = 
     {
         figure : Figure
-        color : bool //true - белый, false - чёрный
+        color : bool //true - white, false - black
     }     
-//Контейнер состояний
+
 type StateStorage = 
     {
         mutable states: List<FigureData[]>
@@ -82,8 +81,8 @@ type StateStorage =
 type MoveInfo = 
     {
         move: (int * int)
-        id: sbyte// -1 = не атакует,
-        fig_num: sbyte //1, 2, 3, 4 = соответствующие фигуры, -1 = нет трансформации, 0 = рокировка
+        id: sbyte// -1 = does not attack,
+        fig_num: sbyte //1, 2, 3, 4 = figure, -1 = no transformation, 0 = castling
     }
     static member Default = 
         {
@@ -93,26 +92,27 @@ type MoveInfo =
         }
 
 module Variables =
-    //Параметры вручную
+    //training parameters
     let epoch = 100
     let learning_rate = 0.0001
     let moment = 0.2
     let discount_factor = 0.8
-    //Константы
+    
     let best_move_count = 10
-    let depth = 40uy//40 ходов фигурами, т.е. 20 ходов всего
+    let depth = 40uy//40 moves with figures, i.e. 20 moves in total
     let eps_count = 70.0
-    //Награды, их видимо надо держать в форме [0;1], активированными по tanh
-    let (b_pawn, b_light_fig, b_heavy_fig, b_queen) = (0.012, 0.036, 0.06, 0.096)//0,18 + 0,14 = 0,32
-    let (b_check, b_mate, b_no_dev) = (0.032, 0.54, 0.002)//0,65 + 0,32 = 0,97
-    //Рейтинг это: сумма собственных фигур - сумма фигур оппонента - состояние собственных фигур + состояние вражеских фигур
-    //шах и мат > любого количества фигур противника
-    //Эпсилон
     let mutable epsilon = 0.0
-    //Статистика
+    
+    //Rewards, they apparently need to be kept in the form [0; 1], activated by tanh
+    let (b_pawn, b_light_fig, b_heavy_fig, b_queen) = (0.012, 0.036, 0.06, 0.096)
+    let (b_check, b_mate, b_no_dev) = (0.032, 0.54, 0.002)
+    
+    //Statistics
     let mutable mid_error = [| 0.0; 0.0 |]
     let (stateStorage: StateStorage[]) = [| StateStorage.Default; StateStorage.Default |]
+    
 module VariablesForIChess =
+    //IChess_Lib use variables from here
     let mutable (weights_rnn_pvb : float[][][]) = [||]
     let mutable (pos_val_pvb: float[,][]) = [||]
     let bot_num = 1uy
